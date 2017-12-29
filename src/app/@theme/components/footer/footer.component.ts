82876104d3011d79b78ff12221870524a01cbfcb
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2017-12-29 16:42:26 -0200

    fix

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -6,10 +6,8 @@ import { Component } from '@angular/core';
   template: `
     <span class="created-by">&copy; 2018, <b><a href="http://www.conttudoweb.com.br" target="_blank">www.conttudoweb.com.br</a></b></span>
     <div class="socials">
-      <!--<a href="#" target="_blank" class="ion ion-social-github"></a>-->
       <a href="https://fb.me/conttudoweb" target="_blank" class="ion ion-social-facebook"></a>
       <a href="https://twitter.com/conttudoweb" target="_blank" class="ion ion-social-twitter"></a>
-      <!--<a href="#" target="_blank" class="ion ion-social-linkedin"></a>-->
     </div>
   `,
 })
